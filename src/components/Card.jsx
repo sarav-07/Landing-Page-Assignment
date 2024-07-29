6e452a038c52489b3a5092bb511f723e1c82e36d
Saravjeet Singh <saravjeetsandhu1999@gmail.com>
2024-07-29 22:02:56 +0530

    Minor changes to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FaRegHeart } from "react-icons/fa";
 import { FaRegComment } from "react-icons/fa6";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
+import profile from "../assets/profile.jpg"
 
 const Card = () => {
   return (
@@ -11,7 +12,7 @@ const Card = () => {
         <h1 className='text-xl font-bold'>Trapped by Tradition: A struggle between family duty and personal despair</h1>
         <p className='pt-2 py-8 font-medium'>Hi, I’m a 32 year old man. I belong from a very lower middle class family but I was very good in studies, so I started living outside home in metro....</p>
         </div>
-        <div className='w-[64px] h-[64px] rounded-full bg-white'></div>
+        <div> <img className='w-[64px] h-[64px] rounded-full ' src={profile} alt="" /></div>
         </div>
   <div className='flex justify-between '>
   <div className=' container flex gap-10  max-w-[530px]'>
